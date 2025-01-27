8612fdd2fb71d318e5b387783b02c839497d58e1
21-kunal <onlyforcc7@gmail.com>
2025-01-27 05:51:00 +0530

    Added rosepine

--- lua/custom/plugins/RosePine.lua
+++ lua/custom/plugins/RosePine.lua
@@ -2,7 +2,7 @@ return {
   'rose-pine/neovim',
   name = 'rose-pine',
   config = function()
-    require('rose-pine').setup { styles = { transparency = true, italic = false, bold = false } }
+    require('rose-pine').setup { variant = 'moon', styles = { transparency = true, italic = false, bold = false } }
 
     vim.cmd.colorscheme 'rose-pine'
   end,
